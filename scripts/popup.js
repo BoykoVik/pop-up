1c926635b778524779ea79765a95eac6237b9d7b
Viktor <gexar47@gmail.com>
2022-08-17 23:52:09 +0300

    begin js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,56 @@
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index];
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute('href').replace('#', '');
+      const curentPopup = document.getElementById(popupName);
+      popupOpen(curentPopup);
+      e.preventDefault();
+    });
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+  for (let i = 0; i < popupCloseIcon.length; i++) {
+    const el = popupCloseIcon[i];
+    el.addEventListener('click', function (e) {
+      popupClose(el.closest('.popup'));//поиск наиверхнего в dom элемента с этим классом
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(curentPopup) {
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    curentPopup.classList.add('open');
+    curentPopup.addEventListener('click', function (e) {
+      if (!e.target.closest('.popup_content')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    });
+  }
+}
+
+
+
+
+
+
 
 k = true;
 $(document).mouseleave(function(e){
@@ -27,5 +80,5 @@ window.onbeforeunload = function (evt) {
     }
     return message;
   }
-
 */
+
