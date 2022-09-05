1e86b794900bc8139084b9016e3e572ba545cbd9
Viktor <gexar47@gmail.com>
2022-09-06 02:28:10 +0300

    vanilla

--- scripts/try.js
+++ scripts/try.js
@@ -7,3 +7,31 @@ objbody.onmouseout = function() {
 }
 
 console.log(objRef)
+
+/*
+objbody.onmouseout = function(){
+  console.log('a');
+  setTimeout(function () {
+    oppp();
+  }, 700);
+  
+  };
+
+function oppp() {
+  if (objbody.onmouseout) {
+    if (k){
+        MicroModal.show('modal-1');
+      k = false;
+  }
+  }
+}
+*/
+setTimeout(function (event2) {
+        
+    if (!event2.target) {
+        if (k){
+            MicroModal.show('modal-1');
+          k = false;
+      }
+      }
+  }, 700);
